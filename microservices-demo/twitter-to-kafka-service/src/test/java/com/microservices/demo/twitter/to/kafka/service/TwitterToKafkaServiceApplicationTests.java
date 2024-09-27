c8ff0e3ddf3b5b79c3c742f35066009cfcebd415
William <wspreitzer2@gmail.com>
2024-09-27 00:55:15 -0500

    Created Application start up functionality with logging keywords and message to console and log file

---
+++ microservices-demo/twitter-to-kafka-service/src/test/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTests.java
@@ -0,0 +1,10 @@
+package com.microservices.demo.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplicationTests {
+    @Test
+    public void contextLoad(){}
+}
